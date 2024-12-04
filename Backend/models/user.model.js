3192e5ea358e7248c0d94f8ce60a0ebb52d53b0e
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-12-04 21:56:46 +0530

    feat: Add user schema with authentication and password hashing

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const JWT = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   fullname: {
@@ -21,8 +23,26 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   socketId: {
     type: String,
   },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
