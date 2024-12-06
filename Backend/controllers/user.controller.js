112595339f0aec9d840ad4377344a2e8a2d5d620
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-12-06 22:05:16 +0530

    chore: showing the logs on console to for debugging

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -8,6 +8,8 @@ module.exports.registerUser = async (req, res, next) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
+  console.log(req.body);
+
   const { fullname, email, password } = req.body;
   const hashPassword = await userModel.hashPassword(password);
   const user = await userService.createUser({
