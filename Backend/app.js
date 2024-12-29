619b3e7d6f4f1c0bc3c199736d45cc32f2f86738
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-12-30 03:22:54 +0530

    feat: Add cookie parser middleware for handling cookies in requests

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,14 @@ dotenv.config();
 const cors = require("cors");
 const connectTODb = require("./database/db");
 const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 
 connectTODb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
